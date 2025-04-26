25d0c136efbdc10932200169973a8b9fa34ec905
Durante101 <durthornton@gmail.com>
2025-04-26 19:29:39 -0400

    Getters-Setters

--- src/main/java/com/pluralsight/NeighborhoodLibrary.java
+++ src/main/java/com/pluralsight/NeighborhoodLibrary.java
@@ -3,5 +3,12 @@ package com.pluralsight;
 public class NeighborhoodLibrary {
     public static void main(String[] args) {
 
+        Book[] inventory = new Book[5];
+        inventory[0] = new Book(1, "1111", "Java Basics");
+        inventory[1] = new Book(2, "2222", "Advanced Java");
+        inventory[2] = new Book(3, "3333", "Data Structures");
+        inventory[3] = new Book(4, "4444", "OOP Concepts");
+        inventory[4] = new Book(5, "5555", "Spring Boot");
+
     }
 }
